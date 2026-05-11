f1e2e05c76b427a00e88890cc7d1d11bf3b65fc6
Chetan A H <hullallichetan4@gmail.com>
2026-05-11 19:37:06 +0530

    Add L7.py

---
+++ L7.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        left = max_length = 0
+        char_set = set()
+        
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
+
+sol = Solution()
+
+print(sol.lengthOfLongestSubstring("abcabcbb"))  # Output: 3
+print(sol.lengthOfLongestSubstring("bbbbb"))     # Output: 1
+print(sol.lengthOfLongestSubstring("pwwkew"))     # Output: 3
